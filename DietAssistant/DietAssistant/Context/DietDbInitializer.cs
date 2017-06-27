ea16ee7ab1ce855d2c6274c551781e24f7d45ff7
Mariia Suvalova <mariia_suvalova@epam.com>
2017-06-27 12:35:18 +0300

    Added data.

--- DietAssistant/DietAssistant/Context/DietDbInitializer.cs
+++ DietAssistant/DietAssistant/Context/DietDbInitializer.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using DietAssistant.Entities;
+using DietAssistant.Core.Enums;
 
 namespace DietAssistant.Context
 {
@@ -14,7 +12,34 @@ namespace DietAssistant.Context
         {
             var dishes = AssignDishes();
 
+            var users = AssignUsers();
+
+            var userDishes = AssignUserDishes(dishes);
+
+            var reports = AssignReports();
+
             db.Dishes.AddRange(dishes);
+
+            db.Users.AddRange(users);
+
+            db.Reports.AddRange(reports);
+
+            db.UserDishes.AddRange(userDishes);
+        }
+
+        private static List<User> AssignUsers()
+        {
+            return new List<User>
+            {
+                new User
+                {
+                    Id = 1,
+                    FirstName = "test-name",
+                    LastName = "test-last-name",
+                    Email = "test@mail.ru",
+                    Type = BodyType.Ectomorph
+                }
+            };
         }
 
         private static List<Dish> AssignDishes()
@@ -48,6 +73,41 @@ namespace DietAssistant.Context
             };
         }
 
+        private static List<Report> AssignReports()
+        {
+            return new List<Report>
+            {
+                new Report
+                {
+                    Id = 1,
+                    Date = DateTime.UtcNow,
+                    UserId=1,
+                    Carbohydrates = 20,
+                    Fats = 30,
+                    Proteins = 40
+                }
+            };
+        }
+
+        private static List<UserDish> AssignUserDishes(List<Dish> dishes)
+        {
+            return new List<UserDish>
+            {
+                new UserDish
+                {
+                    Date = DateTime.UtcNow,
+                    Dish = dishes[0],
+                    Grams = 200,
+                    UserId = 1,
+                },
+                new UserDish
+                {
+                    Date = DateTime.UtcNow,
+                    Dish = dishes[1],
+                    Grams = 200,
+                    UserId = 1
+                }
+            };
+        }
     }
 }
-
