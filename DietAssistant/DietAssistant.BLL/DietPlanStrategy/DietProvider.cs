52de3d63f2c66877e642e33669ef873a962e2e3e
Mariia Suvalova <mariia_suvalova@epam.com>
2017-07-04 08:10:47 +0300

    more fixes

--- DietAssistant/DietAssistant.BLL/DietPlanStrategy/DietProvider.cs
+++ DietAssistant/DietAssistant.BLL/DietPlanStrategy/DietProvider.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using DietAssistant.Core.Enums;
 using DietAssistant.BLL.Infrastructure.Exceptions;
