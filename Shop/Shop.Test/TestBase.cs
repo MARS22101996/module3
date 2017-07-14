54506519c403c9629e37ff63f3c6bae4b5ce2de4
Mariia Suvalova <mariia_suvalova@epam.com>
2017-07-14 08:45:32 +0300

    register

--- Shop/Shop.Test/TestBase.cs
+++ Shop/Shop.Test/TestBase.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNet.Identity;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
 using Moq;
 using Shop.Controllers;
@@ -14,6 +15,8 @@ namespace Shop.Test
             {
                 var userStore = new Mock<IUserStore<ApplicationUser>>();
                 var userManager = new Mock<ApplicationUserManager>(userStore.Object);
+                userManager.Setup(x => x.CreateAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>()))
+                    .Returns(Task.FromResult(IdentityResult.Success));
                 var authenticationManager = new Mock<IAuthenticationManager>();
                 var signInManager =
                     new Mock<ApplicationSignInManager>(userManager.Object, authenticationManager.Object);
